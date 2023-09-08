2834a5f58cca4e5a7925ef3e88833c546be25482
kshithijakappa <kshithijakappa@gmail.com>
2023-09-08 00:37:43 -0400

    Spring Security using JDBCAuthentication

---
+++ SpringBootSecurityJdbcAuthEx/src/main/java/com/app/kshithi/Test.java
@@ -0,0 +1,13 @@
+package com.app.kshithi;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String pwd = encoder.encode("sam");
+		System.out.println(pwd);
+	}
+
+}
